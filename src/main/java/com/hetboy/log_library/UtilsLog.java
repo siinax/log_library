3c530d47b9d1cd0105c813297c021373de5be322
siinax <siinax@foxmail.com>
2017-11-08 12:23:10 +0800

    frist commit

---
+++ src/main/java/com/hetboy/log_library/UtilsLog.java
@@ -0,0 +1,22 @@
+package com.hetboy.log_library;
+
+/**
+ * Created by siinax on 2017/4/7.今天是充满希望的一天!! w(ﾟДﾟ)w (╯°Д°)╯︵ ┻━┻
+ */
+
+abstract class UtilsLog {
+
+    public void Case(String tv){
+        String tag = "---log---";
+        String msg = tv;
+        int i = tv.indexOf("/");
+        if (i != -1) {
+            tag = tv.substring(0, i);
+            msg = tv.substring(i + 1);
+        }
+        log(tag, msg);
+    }
+
+    public abstract void log(String tag, String msg);
+
+}
