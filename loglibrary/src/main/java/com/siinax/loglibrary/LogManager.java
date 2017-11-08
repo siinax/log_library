9fe4adcf39dba678b7e8c2382f84e07c1247d295
siinax <siinax@foxmail.com>
2017-11-08 13:08:54 +0800

    二次提交

---
+++ loglibrary/src/main/java/com/siinax/loglibrary/LogManager.java
@@ -0,0 +1,27 @@
+package com.siinax.loglibrary;
+
+
+/**
+ * Created by siinax on 2017/11/7.今天是充满希望的一天!! w(ﾟДﾟ)w (╯°Д°)╯︵ ┻━┻
+ */
+
+class LogManager {
+
+    private static LogShow mLog;
+
+    public static LogShow Log(){
+        return mLog = mLog == null ? new LogShow() : mLog;
+    }
+
+    /*public static void shutdownLog() {
+        if (mLog != null) {
+            mLog.shutdownLog();
+        }else{
+            throw new IllegalArgumentException("异常:mLog == null");
+        }
+    }*/
+
+    public static void recycle(){
+        mLog = mLog != null ? null : null;
+    }
+}
